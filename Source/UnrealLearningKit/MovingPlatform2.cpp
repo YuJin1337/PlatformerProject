46fb11734b52d549ab767cac8952b46cfdc1a7ac
Evgenator <sluzhaev.e@panoramikltd.com>
2024-11-23 14:39:28 +0300

    Importing all the starter kit stuff

---
+++ Source/UnrealLearningKit/MovingPlatform2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform2.h"
+
+MovingPlatform2::MovingPlatform2()
+{
+}
+
+MovingPlatform2::~MovingPlatform2()
+{
+}
---
+++ Source/UnrealLearningKit/MovingPlatform2.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UNREALLEARNINGKIT_API MovingPlatform2
+{
+public:
+	MovingPlatform2();
+	~MovingPlatform2();
+};
